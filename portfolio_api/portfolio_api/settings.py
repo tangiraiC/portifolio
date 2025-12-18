d72f4221ecfc72c3448f81e68814c1767b2292f6
Lincoln Chanakira <lincolnchanakira@gmail.com>
2025-12-18 13:54:39 -0500

    Configure for Railway deployment (Whitenoise, Procfile, requirements)

--- portfolio_api/portfolio_api/settings.py
+++ portfolio_api/portfolio_api/settings.py
@@ -56,6 +56,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -142,6 +143,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # DRF defaults (open for now; lock down later)
 REST_FRAMEWORK = {
